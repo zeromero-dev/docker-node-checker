f705928a5b0ffb4194a2e0348b34f549b9503a4c
Roman <spiderr.roma@gmail.com>
2022-01-16 01:55:28 +0200

    oh my...

--- index.js
+++ index.js
@@ -11,34 +11,54 @@
     (async () => {
         const username = 'ovasylenko'
         let userUrl = `https://api.github.com/users/${username}/repos`
-        
+        const regex = /[a-zA-Z].Dockerfile/g
 
         let repoArray = await axios.get(userUrl)
             .then(response => {
                var repos = response.data.map(it => it.name)
             console.log(repos) 
 
-            for (let i = 0; i < 2; i+=1) {
+            for (let i = 0; i < 1; i+=1) { //change 2 to repos.length
                 let repoUrl = `https://api.github.com/repos/${username}/${repos[i]}/git/trees/master?recursive=1`
                     axios.get(repoUrl)
                     .then(response => {
                      let dataArray = response.data //response.data
+                     let dataTree = dataArray.tree
+                     let filteredFolder = dataTree.filter((it) => {
+                         return it.path === 'docker'
+                     })
+                    //  let dockerTree = filteredFolder.tree
+                    //  let fileTree = dockerTree.filter(it => {
+                    //      return (it.path).match(regex)
+                    //  })
 
-                    //  for(let k = 0; k < dataArray.length; k++){
-                    //     let mappedPath = dataArray.map(it => it.path)
+                    //  let mappedPath = dataTree.map(it => it.path)
+                    //     let filtered =  mappedPath.filter((current, index, array) => {
+                    //        mappedPath === ''
+                    //     })
+
+
+                    //  for(let k = 0; k < dataTree.length; k++){
+                    //     let acc = []
+                    //     let mappedPath = dataTree.map(it => it.path)
                     //     if(mappedPath === '//regulartd'){
-                    //         return dataArray[k] //and go to the objects url
+                    //         dataTree[k] += acc //and go to the objects url
                     //     }
                     //  }
                      
                      
-                    console.log(dataArray) 
+                    console.log(dataTree) 
                 })
             }
     })
     .catch(err => {
         console.log(err);
     })
+
+    // function firstLineChecker(){
+    //     for(let i = 0; i < ;i++)
+    // }
+
 
     function b64_to_utf8( str ) {
         return decodeURIComponent(escape(window.atob( str )));
