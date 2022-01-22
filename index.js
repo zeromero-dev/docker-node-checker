ec3d42612c9357a247c8bf096967a49c124d94ea
Roman <spiderr.roma@gmail.com>
2022-01-22 17:10:33 +0200

    reduced api calls to first 10

--- index.js
+++ index.js
@@ -12,13 +12,12 @@
             .then(response => {
                var repos = response.data.map(it => it.name)
             console.log(repos) 
-                //https://api.github.com/search/code?q=${searchingString}+in:file+filename:${fileName}+repo:${username}/${repos[i]}
-            for (let i = 0; i < 1; i+=1) { //change 2 to repos.length
-                // let repoUrl = `https://api.github.com/repos/${username}/${repos[i]}/git/trees/master?recursive=1`
-                    let filteredFiles = `https://api.github.com/search/code?q=${searchingString}+in:file+filename:${fileName}+repo:${username}/skillcrucial-react-redux-boilerplate` //change to ${repos[i]}
+               
+            for (let i = 0; i < 10; i+=1) { //change 2 to repos.length
+                    let filteredFiles = `https://api.github.com/search/code?q=${searchingString}+in:file+filename:${fileName}+repo:${username}/${repos[i]}` //change to ${repos[i]} //skillcrucial-react-redux-boilerplate
                     axios.get(filteredFiles)
                     .then(response => {
-                     let dataArray = response.data.items//response.data
+                     let dataArray = response.data.items
                      let mappedFile = dataArray.map(it => (it.url))
                      console.log(mappedFile)
 
@@ -28,9 +27,9 @@
                             let content =  response.data.content
                             let buff = Buffer.from(content, 'base64');
                             let decoded = buff.toString('ascii')
-                        //    console.log(decoded)
+                      
                            let resultArr = decoded.match(regex)
-                           let result = resultArr.join(' \n') + `${repos[i]}`
+                           let result = resultArr.join(' \n') + `\n skillcrucial-react-redux-boilerplate \n` //change to ${repos[i]}
                            console.log(result)
                            let writeToFile = fs.appendFile('./results/results.txt', result, (err) => {
                             console.log("ERROR: ", err)
