d2cf0c99fab9e22f7caeb1b1b22e361e7dea7ed4
Roman <spiderr.roma@gmail.com>
2023-06-07 17:44:03 +0300

    cleaned up but not working

--- index.js
+++ index.js
@@ -1,46 +1,39 @@
-    const axios = require('axios');
-    const fs = require('fs');
-   
-    (async () => {
-        const username = 'ovasylenko'
-        let userUrl = `https://api.github.com/users/${username}/repos`
-        const regex = /FROM\s+node\:\w+/gm
-        const fileName = '.Dockerfile'
-        const searchingString = 'node:'
+const axios = require("axios");
+const fs = require("fs");
 
-        let repoArray = await axios.get(userUrl)
-            .then(response => {
-               var repos = response.data.map(it => it.name)
-            console.log(repos) 
-               
-            for (let i = 0; i < 10; i+=1) { //change 2 to repos.length
-                    let filteredFiles = `https://api.github.com/search/code?q=${searchingString}+in:file+filename:${fileName}+repo:${username}/${repos[i]}` //change to ${repos[i]} //skillcrucial-react-redux-boilerplate
-                    axios.get(filteredFiles)
-                    .then(response => {
-                     let dataArray = response.data.items
-                     let mappedFile = dataArray.map(it => (it.url))
-                     console.log(mappedFile)
+(async () => {
+  const username = "ovasylenko";
+  const userUrl = `https://api.github.com/users/${username}/repos`;
+  const regex = /FROM\s+node\:\w+/gm;
+  const fileName = ".Dockerfile";
+  const searchingString = "node:";
 
-                     for(let k = 0; k < mappedFile.length ;k++){
-                         axios.get(mappedFile[k])
-                         .then(response => {
-                            let content =  response.data.content
-                            let buff = Buffer.from(content, 'base64');
-                            let decoded = buff.toString('ascii')
-                      
-                           let resultArr = decoded.match(regex)
-                           let result = resultArr.join(' \n') + `\n ${repos[i]} \n` //change to ${repos[i]}
-                           console.log(result)
-                           let writeToFile = fs.appendFile('./results/results.txt', result, (err) => {
-                            console.log("ERROR: ", err)
-                          })
-                         }) 
-                     }
-                })
-            }
-    })
-    .catch(err => {
-        console.log(err);
-    })
-})()
-    
+  try {
+    const response = await axios.get(userUrl);
+    const repos = response.data.map((it) => it.name);
+
+    for (let i = 0; i < repos.length; i += 1) {
+      const filteredFiles = `https://api.github.com/search/code?q=${searchingString}+in:file+filename:${fileName}+repo:${username}/${repos[i]}`;
+      const filteredFilesResponse = await axios.get(filteredFiles);
+      const dataArray = filteredFilesResponse.data.items;
+      const mappedFile = dataArray.map((it) => it.url);
+
+      for (let k = 0; k < mappedFile.length; k++) {
+        const fileResponse = await axios.get(mappedFile[k]);
+        const content = fileResponse.data.content;
+        const buff = Buffer.from(content, "base64");
+        const decoded = buff.toString("ascii");
+        const resultArr = decoded.match(regex);
+        const result = resultArr.join(" \n") + `\n ${repos[i]} \n`;
+        console.log(result);
+        fs.appendFile("./results/results.txt", result, (err) => {
+          if (err) {
+            console.log("ERROR: ", err);
+          }
+        });
+      }
+    }
+  } catch (err) {
+    console.log(err);
+  }
+})();
